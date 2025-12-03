9bf2977b2e47c17ed7626481d821c3a0c3007c05
rafiei <rafieiorg4@gmail.com>
2025-12-03 16:01:56 +0330

    final

--- test.cpp
+++ test.cpp
@@ -1,16 +1,40 @@
 #include <iostream>
-#include <random>
+#include <cstdlib>
+#include <cppconn/driver.h>
+#include <cppconn/connection.h>
+#include <cppconn/exception.h>
 using namespace std;
-int main()
-{
-    char chars[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+using namespace sql;
 
-    random_device rd;
-    string token;
-    for(int i=0; i < 30 ; ++i)
-    {
-        token += chars[rd() % sizeof(chars) - 2];
+int main() {
+    const char* env = getenv("ENV");
+    string dbHost, dbName, dbUser, dbPass;
+
+    if (env && string(env) == "production") {
+        dbHost = "tcp://127.0.0.1:3306";
+        dbName = "testdb";
+        dbUser = "testuser";
+        dbPass = "123456";
+        cout << "Running in Production environment" << endl;
+    } else {
+        dbHost = "127.0.0.1";
+        dbName = "todoList";
+        dbUser = "root";
+        dbPass = "123456";
+        cout << "Running in Sandbox environment" << endl;
     }
-    cout << token << endl;
 
+    try {
+        Driver* driver = get_driver_instance();
+        Connection* con = driver->connect(dbHost, dbUser, dbPass);
+        con->setSchema(dbName);
+        PreparedStatement* pstmt = con->prepareStatement("SELECT username, role FROM users");
+
+        cout << "Connected to database: " << dbName << endl;
+        delete con;
+    } catch (SQLException &e) {
+        cerr << "Error connecting to database: " << e.what() << endl;
+    }
+
+    return 0;
 }
